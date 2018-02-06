a7e4b75bbfa40418d649c79114a1da7148808e3d
leon-v <leon@valkenb.org>
2018-02-07 08:16:07 +1300

    Moved config GHTML off chip
    Oh wait.. I Can't do that :-(
    Will fix later.

--- src/modules/http_config.c
+++ src/modules/http_config.c
@@ -8,6 +8,7 @@
 #include "osapi.h"
 #include "espconn.h"
 #include "mem.h"
+#include "config.h"
 
 LOCAL struct espconn esp_conn;
 LOCAL esp_tcp esptcp;
@@ -41,7 +42,6 @@ LOCAL void HTTPConfig_TCPServerSendResponse(struct espconn *pespconn, int error,
 }
 
 
-
 LOCAL void HTTPConfig_TCPServerRecieveCallback(void *arg, char *pusrdata, unsigned short length) {
 
 	char *ptr = 0;
@@ -54,21 +54,28 @@ LOCAL void HTTPConfig_TCPServerRecieveCallback(void *arg, char *pusrdata, unsign
 
 	ptr[0] = '\0';
 	if (os_strcmp(pusrdata, "GET / HTTP/1.1") == 0) {
-		HTTPConfig_TCPServerSendResponse(pespconn, 200, "<!DOCTYPE html>\
-<html>\
-<head>\
-	<title>ESP8266 V-Config</title>\
-</head>\
-<body>\
-	<form method=\"post\">\
-		<input type=\"submit\">\
-	</form>\
-</body>\
-</html>");
+		char *html = NULL;
+		html = (char *) os_malloc(1024 * sizeof(char));
+		os_sprintf(html, "<!DOCTYPE html>\
+			<html>\
+			<head>\
+				<title>ESP8266 V-Config</title>\
+			</head>\
+			<body>\
+				<object data=\"https://esp8266.valkenb.org/config/v1/\"></object>\
+		</html>");
+		HTTPConfig_TCPServerSendResponse(pespconn, 200, html);
 	}
-	
+	else if (os_strcmp(pusrdata, "GET /data HTTP/1.1") == 0) {
+		CFG_Load();
+		//sysCfg.sta_ssid, sysCfg.sta_pwd
+		HTTPConfig_TCPServerSendResponse(pespconn, 404, "");
+	}
 	else if (os_strcmp(pusrdata, "POST / HTTP/1.1") == 0) {
-		
+		HTTPConfig_TCPServerSendResponse(pespconn, 404, "");
+	}
+	else{
+		HTTPConfig_TCPServerSendResponse(pespconn, 404, "");
 	}
 }
 
@@ -105,28 +112,42 @@ LOCAL void HTTPConfig_TCPPortConnected(void *arg) {
 
 void HTTPConfig_Init(void){
 
-	struct softap_config config;
+	// struct softap_config config;
 
-	wifi_softap_get_config(&config); // Get config first.
+	// wifi_softap_get_config(&config); // Get config first.
 
-	os_memset(config.ssid, 0, 32);
-	os_memset(config.password, 0, 64);
-	os_memcpy(config.ssid, "ESP8266 V-Sensor Config Mode", 28);
-	os_memcpy(config.password, "", 0);
-	config.authmode = AUTH_OPEN;
-	config.ssid_len = 28;// or its actual length
-	config.beacon_interval = 100;
-	config.max_connection = 1; // how many stations can connect to ESP8266 softAP at most.
+	// os_memset(config.ssid, 0, 32);
+	// os_memset(config.password, 0, 64);
+	// os_memcpy(config.ssid, "ESP8266 V-Sensor Config Mode", 28);
+	// os_memcpy(config.password, "", 0);
+	// config.authmode = AUTH_OPEN;
+	// config.ssid_len = 28;// or its actual length
+	// config.beacon_interval = 100;
+	// config.max_connection = 1; // how many stations can connect to ESP8266 softAP at most.
 
-	wifi_softap_set_config(&config);// Set ESP8266 softap config .
+	// wifi_softap_set_config(&config);// Set ESP8266 softap config .
 
-	os_printf("WiFi AP Started \r\n");
+	// os_printf("WiFi AP Started \r\n");
 
-	uint8 mode = 0;
-	wifi_softap_set_dhcps_offer_option(OFFER_ROUTER, &mode);
+	// uint8 mode = 0;
+	// wifi_softap_set_dhcps_offer_option(OFFER_ROUTER, &mode);
 
-	os_printf("DHCP Started \r\n");
+	// os_printf("DHCP Started \r\n");
 
+
+	os_printf("WIFI_INIT\r\n");
+	wifi_set_opmode_current(STATION_MODE);
+
+	struct station_config stationConf;
+	os_memset(&stationConf, 0, sizeof(struct station_config));
+
+	os_sprintf(stationConf.ssid, "%s", WIFI_SSID);
+	os_sprintf(stationConf.password, "%s", WIFI_PASS);
+
+	wifi_station_set_config_current(&stationConf);
+
+	wifi_station_set_auto_connect(TRUE);
+	wifi_station_connect();
 
 
 
